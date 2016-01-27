b3f2be8441611191d3281286a83b426d85214329
Yu Lei <326060431@qq.com>
2016-01-26 23:31:56 -0800

    update proto.lua

--- cat/proto.lua
+++ cat/proto.lua
@@ -43,6 +43,60 @@ proto.c2s = sprotoparser.parse [[
     finished 1 : integer
 }
 
+.mail
+{	
+		emailid 0 : integer
+		type 1 : integer
+		iconid 2 : integer
+		acctime 3 : string
+		isread 4 : boolean
+		isreward 5 : boolean
+		title 6 : string
+		content 7 : string
+		error 8 : integer
+		
+		.attach
+		{
+			itemsn 0 : integer
+			itemnum 1 : integer
+		}
+		
+		attachs 9 : *attach
+}
+.idlist
+{
+    id 0 : integer
+}
+
+.subuser
+{
+	id 0 : integer
+	name 1 : string
+	level 2 : integer
+	viplevel 3 : integer
+	iconid 4 : integer
+	sign 5 : string
+	fightpower 6  : integer
+	qianming 7 : string
+	uid  8 :string
+	online_time 9 : string
+	heart 10 : boolean 
+	apply 11 : boolean 
+	receive 12 : boolean 
+}
+
+.apply
+{
+	id 0 : integer
+}
+
+.heart
+{
+	id 0 : integer
+	heart 1 : integer
+}
+
+
 handshake 1 {
     request {
         secret 0 : string
@@ -63,18 +117,12 @@ role 2 {
     }
 }
 
-mail 3 {
-    request {
-        from 0 : integer
-        to 1 : integer
-        title 2 : string
-        msg 3 : string
-    }
-    response {
-        ok 0 : boolean 
-        error 1 : integer
-        msg 2 : string
-    }
+mails 3 {
+  response {
+    ok 0 : boolean
+	msg 1 : string 
+	mail_list 2 : *mail	
+  }
 }
 
 signup 4 {
@@ -157,9 +205,29 @@ achievement 11 {
         msg 1 : string  
         achis 2 : *achi
     }
+} 	 
+
+mail_read 12
+{
+	request { 
+        mail_id 0 : *idlist
+    }
 }
 
-channel 12 {}
+mail_delete 13
+{
+	request { 
+		mail_id 0 : *idlist
+	}
+}
+
+mail_getreward 14
+{
+	request { 
+		mail_id 0 : *idlist
+		type 1 : integer
+	}
+} 
 ]]
 
 proto.s2c = sprotoparser.parse [[
