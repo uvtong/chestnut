995c0bb9b4a3bd1a031400305ca9b5e83ea03d4c
mephostopilsi <mephostopilis@gmail.com>
2016-02-21 23:47:45 +0800

    xx

--- test.sh
+++ test.sh
@@ -3,8 +3,4 @@
 #echo $1
 #echo $2
 cd skynet
-<<<<<<< HEAD:master.sh~
-./skynet ./../aa/config
-=======
-./3rd/lua/lua ./../crazy/testclient.lua
->>>>>>> 7d02abb1fc92d69ba8c0ba9dc1edbc73d31b4b47:test.sh
+./3rd/lua/lua ./../test.lua
