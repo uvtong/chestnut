89a35e914ce632c7f56fefdf774222f3787c0144
me <mephostopilis@gmail.com>
2016-02-22 20:34:17 +0800

    xx

---
+++ cat/models/g_role_starmgr.lua
@@ -0,0 +1,86 @@
+local skynet = require "skynet"
+local util = require "util"
+
+local _M = {}
+_M.__data = {}
+_M.__count = 0
+
+local _Meta = { star=0, up_prop_num=0, csv_id=0, gather_buffer_id=0, battle_buffer_id=0}
+
+_M.__tname = "g_role_star"
+
+function _Meta.__new()
+ 	-- body
+ 	local t = {}
+ 	setmetatable( t, { __index = _Meta } )
+ 	return t
+end 
+
+function _Meta:__insert_db()
+	-- body
+	local t = {}
+	for k,v in pairs(self) do
+		if not string.match(k, "^__*") then
+			t[k] = self[k]
+		end
+	end
+	skynet.send(util.random_db(), "lua", "command", "insert", self.__tname, t)
+end
+
+function _Meta:__update_db(t)
+	-- body
+	assert(type(t) == "table")
+	local columns = {}
+	for i,v in ipairs(t) do
+		columns[tostring(v)] = self[tostring(v)]
+	end
+	skynet.send(util.random_db(), "lua", "command", "update", self.__tname, {{ id = self.id }}, columns)
+end
+
+function _Meta:__serialize()
+	-- body
+	local r = {}
+	for k,v in pairs(_Meta) do
+		if not string.match(k, "^__*") then
+			r[k] = self[k]
+		end
+	end
+	return r
+end
+
+function _M.create( P )
+	assert(P)
+	local u = _Meta.__new()
+	for k,v in pairs(_Meta) do
+		if not string.match(k, "^__*") then
+			u[k] = assert(P[k])
+		end
+	end
+	return u
+end	
+
+function _M:add( u )
+	assert(u)
+	self.__data[tostring(u.star)] = u
+	self.__count = self.__count + 1
+end
+	
+function _M:get_by_star(star)
+	-- body
+	return self.__data[tostring(star)]
+end
+
+function _M:delete_by_star(star)
+	-- body
+	assert(self.__data[tostring(star)])
+	self.__data[tostring(star)] = nil
+	self.__count = self.__count - 1
+end
+
+function _M:get_count()
+	-- body
+	return self.__count
+end
+
+return _M
+
