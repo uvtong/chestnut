12601d6693002a265b8849249dbef161f13bdd46
mephostopilsi <mephostopilis@gmail.com>
2016-03-07 09:53:10 +0800

    xx

--- host/errorcode.lua
+++ host/errorcode.lua
@@ -3,5 +3,8 @@ local errorcode = {}
 errorcode[1] = { code = 1, msg = "success"}
 errorcode[2] = { code = 2, msg = "offline"}
 errorcode[3] = { code = 3, msg = "ready and deal cards"}
+errorcode[4] = { code = 4, msg = "deal cards and rob"}
+errorcode[5] = { code = 5, msg = "lead and game over"}
+errorcode[6] = { code = 5, msg = "mp and deal cards"}
 
 return errorcode
