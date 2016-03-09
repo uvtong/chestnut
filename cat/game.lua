946eb65dc1f60cd563f22a2274d7caff4625449e
me <mephostopilis@gmail.com>
2016-03-09 20:36:45 +0800

    xx

--- cat/game.lua
+++ cat/game.lua
@@ -10,7 +10,66 @@ local CMD = {}
 function CMD.start()
  	-- body
  	game = loader.load_game()
- end 
+end 
+
+function CMD.query(table_name, pk)
+	-- body
+	for k,v in pairs(game) do
+		if v.__tname == table_name then
+			return assert(v:get_by_csv_id(pk))
+		end
+	end
+end
+
+function CMD.query_g_goods(pk)
+	-- body
+	if type(pk) == "number" then
+		return game.g_goodsmgr:get_by_csv_id(pk)
+	elseif type(pk) == "nil" then
+		return game.g_goodsmgr.__data
+	elseif type(pk) == "table" then
+		local r = {}
+		for i,v in ipairs(pk) do
+			local t = assert(game.g_goodsmgr:get_by_csv_id(v))
+			table.insert(r, t)
+		end
+		return r
+	else
+		assert(false)
+	end
+end
+
+function CMD.query_g_goods_refresh_cost(pk)
+	-- body
+	if type(pk) == "number" then
+		return game.g_goods_refresh_costmgr:get_by_csv_id(pk)
+	elseif type(pk) == "nil" then
+		return game.g_goods_refresh_costmgr.__data
+	else
+		assert(false)
+	end
+end
+
+function CMD.query_g_prop(pk)
+	-- body
+	if type(pk) == "number" then
+		return game.g_propmgr:get_by_csv_id(pk)
+	elseif type(pk) == "nil" then
+		return game.g_propmgr.__data
+	else
+		assert(false)
+	end
+end
+
+function CMD.u_guid(user_id, game, csv_id)
+	-- body
+	return util.u_guid(user_id, game, csv_id)
+end
+
+function CMD.guid(game, csv_id)
+	-- body
+	return util.guid(game, csv_id)
+end
 
 skynet.start(function()
 	skynet.dispatch("lua", function(_,_, command, ...)
