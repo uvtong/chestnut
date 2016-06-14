f183b614c050222864af63292de94dcab961aa79
me <mephostopilis@gmail.com>
2016-06-14 17:19:57 +0800

    fix

--- lualib/common.lua
+++ lualib/common.lua
@@ -10,3 +10,11 @@ function genpk_2(_1, _2, ... )
     pk = (pk | (~(1 << 33 -1) & _2 ))
     return pk
 end
+
+function genpk_3(_1, _2, ... )
+	-- body
+	local pk = (~(1 << 9 -1) & _1)
+    pk = pk << 24
+    pk = (pk | (~(1 << 25 -1) & _2 ))
+    return pk
+end
