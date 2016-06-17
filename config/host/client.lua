619cea5503dbe73961ddb17ba1d17fcfeb0e886d
me <mephostopilis@gmail.com>
2016-06-17 21:52:16 +0800

    Fix

--- config/host/client.lua
+++ config/host/client.lua
@@ -1,7 +1,8 @@
-package.cpath = "luaclib/?.so"
-package.path = "./../cat/?.lua;lualib/?.lua"
+package.cpath = "./../../skynet/luaclib/?.so;./../../lua-socket/?.so"
+package.path = "./../../skynet/lualib/?.lua"
 
 local socket = require "clientsocket"
+local packagesocket = require "packagesocket"
 local crypt = require "crypt"
 local proto = require "proto"
 local sproto = require "sproto"
@@ -10,7 +11,7 @@ if _VERSION ~= "Lua 5.3" then
 	error "Use lua 5.3"
 end
 
-local fd = assert(socket.connect("192.168.1.239", 3002))
+-- local fd = assert(socket.connect("192.168.1.239", 3002))
 
 local function writeline(fd, text)
 	socket.send(fd, text .. "\n")
@@ -59,6 +60,62 @@ local readline = unpack_f(unpack_line)
 -- 0
 --local cha = crypt.base64decode(readline())
 -- print(readline())
+local token = {
+	server = "sample",
+	user = "125",
+	pass = "123",
+}
+
+local function encode_token(token)
+	return string.format("%s@%s:%s",
+		crypt.base64encode(token.user),
+		crypt.base64encode(token.server),
+		crypt.base64encode(token.pass))
+end
+
+local clientkey
+local challenge
+local secret
+local g = packagesocket.new()
+local fd = packagesocket.socket(g)
+ssert(packagesocket.connect("192.168.1.239", 3002) == packagesocket.SOCKET_OPEN)
+local step = 1
+while true do 
+	local t = packagesocket.poll(g)
+	if t[fd] then
+		local line = t[fd]
+	end
+	if line then
+		if step == 1 then
+			challenge = crypt.base64decode(line)
+			step = step + 1
+		elseif step == 2 then
+			clientkey = crypt.randomkey()
+			packagesocket.sendline(g, fd, crypt.base64encode(crypt.dhexchange(clientkey)))
+			step = step + 1
+		elseif step == 3 then
+			secret = crypt.dhsecret(crypt.base64decode(line), clientkey)
+			print("sceret is ", crypt.hexencode(secret))
+			local hmac = crypt.hmac64(challenge, secret)
+			packagesocket.sendline(g, fd, crypt.base64encode(hmac))
+			step = step + 1
+		elseif step == 4 then
+			local etoken = crypt.desencode(secret, encode_token(token))
+			local b = crypt.base64encode(etoken)
+
+			packagesocket.sendline(g, fd, crypt.base64encode(etoken))
+			step = step + 1
+		elseif step == 5 then
+			local code = tonumber(string.sub(line, 1, 3))
+			assert(code == 200)
+			packagesocket.closesocket(g, fd)
+		end
+	end
+end
+
+
+
+
 
 -- 1. get challenge
 local challenge = crypt.base64decode(readline())
@@ -249,18 +306,36 @@ local function dispatch_package()
 	end
 end
 
+
+local pg
+
+while true do 
+	packagesocket.poll(g)
+end
+
 --local text = "echo"
 local index = 1
+
+
 
 print("connect")
 fd = assert(socket.connect("192.168.1.239", 3301))
 last = ""
+
+
 
 local handshake = string.format("%s@%s#%s:%d", crypt.base64encode(uid), crypt.base64encode(token.server),crypt.base64encode(subid) , index)
 local hmac = crypt.hmac64(crypt.hashkey(handshake), secret)
 
 -- send handshake
 send_package(fd, handshake .. ":" .. crypt.base64encode(hmac))
+
+
+
+local step = 1
+while true do 
+
+end
 
 print(readpackage())
 while true do
